34cc0e81aeacddd65fc1d7e0c395831f2b2cf098
Maria Husmann <maria.husmann@gmail.com>
2019-03-08 11:52:30 +0100

    working locallz

--- server.js
+++ server.js
@@ -4,35 +4,45 @@ const http = require('http').Server(app);
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const nconf = require('nconf');
+const Multer = require('multer');
+const bodyParser = require('body-parser');
+const { Storage } = require('@google-cloud/storage');
+const format = require('util').format;
+
+require('dotenv').config();
+
+// Instantiate a storage client
+const storage = new Storage();
 
 
 // Read in keys and secrets. Using nconf use can set secrets via
 // environment variables, command-line arguments, or a keys.json file.
 nconf.argv().env().file('keys.json')
 
-
-
 const user = nconf.get('mongoUser');
 const pass = nconf.get('mongoPass');
 const db = nconf.get('mongoDB');
 const cluster = nconf.get('mongoCluster');
 
+
 const uri = `mongodb+srv://${user}:${pass}@${cluster}/${db}?retryWrites=true`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
     console.log(err);
-  const collection = client.db(db).collection("photos");
-  // perform actions on the collection object
-  let photo = { name: "image.jgp", date: new Date()};
-  collection.insertOne(photo, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-  } );
+    const collection = client.db(db).collection("photos");
 
+    /*
+    // perform actions on the collection object
+    let photo = { name: "image.jgp", date: new Date()};
+    collection.insertOne(photo, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+    } );
   
-  client.close();
+    */
+    //  client.close();
 });
 
 
@@ -44,6 +54,68 @@ app.get('/', function (req, res) {
 // serve all files in dist
 app.use(express.static('dist'));
 
-http.listen(process.env.PORT || 8080, function(){
+
+// A bucket is a container for objects (files).
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+
+app.use(bodyParser.json());
+// Multer is required to process file uploads and make them available via
+// req.files.
+const multer = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 10 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+    },
+});
+
+app.post('/uploads', multer.single('file'), (req, res, next) => {
+    console.log("uploading");
+    if (!req.file) {
+        console.log("no file");
+        res.status(400).send('No file uploaded.');
+        return;
+    }
+
+    console.log(req.file.originalname);
+
+    // Create a new blob in the bucket and upload the file data.
+    const blob = bucket.file(req.file.originalname);
+    const blobStream = blob.createWriteStream({
+        resumable: false,
+    });
+
+    blobStream.on('error', err => {
+        console.log(err);
+        next(err);
+    });
+
+    blobStream.on('finish', () => {
+        // The public URL can be used to directly access the file via HTTP.
+        const publicUrl = format(`https://storage.googleapis.com/${bucket.name}/${blob.name}`);
+        res.status(200).send(publicUrl);
+
+        // Add to MongoDB
+        const collection = client.db(db).collection("photos");
+        // perform actions on the collection object
+        let photo = {
+            name: blob.name,
+            date: new Date(),
+            url: publicUrl
+        };
+        collection.insertOne(photo, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+        });
+
+    });
+
+    blobStream.end(req.file.buffer);
+
+});
+
+
+http.listen(process.env.PORT || 8080, function () {
     console.log(`listening on *: ${http.address().port}`);
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,50 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input
+      type="file"
+      id="imageupload"
+      name="imageupload"
+      accept="image/png, image/jpeg"
+      ref="fileinput"
+    >
+    <button @click="upload">Upload</button>
+    <img :src="cloudimage">
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
+  data: function() {
+    return {
+      files: null,
+      cloudimage: ""
+    };
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    upload: function() {
+      let data = new FormData();
+      data.append("file", this.$refs.fileinput.files[0]);
+
+      fetch("/uploads", {
+        method: "POST",
+        body: data
+      })
+      .then(response => {
+        return response.text();
+      })
+      .then(mytext => {
+        console.log(mytext);
+        this.cloudimage = mytext;
+      });
+    }
   }
-}
+};
 </script>
